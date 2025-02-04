98b7aa3592dd7ecfc20d104122cbf0a20048aded
Oleh <sholeg2005@gmail.com>
2025-02-04 16:58:26 +0100

    add home page for restaurant page

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Cook, Dish, DishType
 
-# Create your views here.
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = "restaurant/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['num_cooks'] = Cook.objects.count()
+        context['num_dishes'] = Dish.objects.count()
+        context['num_dish_types'] = DishType.objects.count()
+
+        num_visits = self.request.session.get('num_visits', 0)
+        self.request.session['num_visits'] = num_visits + 1
+        context['num_visits'] = num_visits + 1
+
+        return context
