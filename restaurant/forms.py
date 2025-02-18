0dd727ba9d42c66cc4ad04a22c0fbdb3222cf78d
Oleh <sholeg2005@gmail.com>
2025-02-18 17:38:31 +0100

    add forms for cook create and update

---
+++ restaurant/forms.py
@@ -0,0 +1,53 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+from restaurant.models import Cook, Dish
+
+
+class CookYearsOfExperienceCreateForm(forms.ModelForm):
+    class Meta:
+        model = Cook
+        fields = UserCreationForm.Meta.fields + (
+            "years_of_experience",
+            "first_name",
+            "last_name",
+        )
+
+    def clean_years_of_experience(self):
+        years_of_experience = self.cleaned_data["years_of_experience"]
+        if years_of_experience <= 0:
+            raise forms.ValidationError("Can't be less than 0")
+        if years_of_experience > 100:
+            raise forms.ValidationError("Can't be more than 100")
+        if not years_of_experience.isdigit():
+            raise forms.ValidationError("Can't be not a digit")
+        return years_of_experience
+
+
+class CookYearsOfExperienceUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Cook
+        fields = ["years_of_experience"]
+
+    def clean_years_of_experience(self):
+        years_of_experience = self.cleaned_data["years_of_experience"]
+        if years_of_experience <= 0:
+            raise forms.ValidationError("Can't be less than 0")
+        if years_of_experience > 100:
+            raise forms.ValidationError("Can't be more than 100")
+        if not years_of_experience.isdigit():
+            raise forms.ValidationError("Can't be not a digit")
+        return years_of_experience
+
+
+class DishForm(forms.ModelForm):
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    class Meta:
+        model = Dish
+        fields = "__all__"
